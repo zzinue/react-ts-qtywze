84ce4730ad665aa03233e8ccbf8193055b81a05d
zzinue <zzinue@gmail.com>
2022-05-10 23:16:04 -0500

    "class about Router"

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {Home} from './pages/Home'
 1.- tiene una pagina home donde nos muestra los primeros 20 personajes
 2.- Tiene una pagina about donde habla de ustedes como devs ( hacer un parrafo y foto)
 3.- Agregar estilos
-2.- al hacer click a un personaje me direcciona a una pantalla de detalle del personaje
+2.- al hacer click en cada card de un personaje me direcciona a una pantalla de detalle del personaje
 
 Retos:investigar paginacion e implemetarla en home
 preguntar por el infiniti scroll
--- Pages/Home.js
+++ Pages/Home.js
@@ -3,20 +3,23 @@ import axios from 'axios'
 
 const URL = 'url de rick y morti'
 export const Home =()=>{
-  const [character, setCharacter]=React.useState([]) 
+  const [characters, setCharacter]=React.useState([]) 
 
   useEffect(async()=>{
-    const {data}=await axios (URL)
-    console.log(data)
-    setCharacter(data.results)
+   fetch(URL)
+    .then((res)=>res.json())
+    .then((body)=>setCharacter([...characters, body.results]))
   },[])
+  console.log(characters)
+  //refactoriza el elementos character item
   return (
     <>
   <h1>Rick and Morty</h1>
   <div className="Home-content result">
-    {
-
-    }
+    characters.map(character=>(
+      <div> key={character.id}</div>))
+    <p>{character.name}</p>
+    <img src="{character.image}" alt="{character.name}">
   </div>
     </>
   )
